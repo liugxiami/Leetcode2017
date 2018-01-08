b12e10b0b6e2dee1020c0ca52b1d76d29cf11713
Sean <xiamiliu@gmail.com>
2018-01-07 21:03:21 -0600

    4的倍数不能赢

---
+++ src/com/ccsi/leetcode2/LC290NimGame.java
@@ -0,0 +1,10 @@
+package com.ccsi.leetcode2;
+
+/**
+ * Created by gxliu on 2018/1/7.
+ */
+public class LC290NimGame {
+    public static boolean canWinNim(int n){
+        return (n%4)!=0;
+    }
+}
