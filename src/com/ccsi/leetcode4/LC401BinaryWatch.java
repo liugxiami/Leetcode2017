d299c1bf2aeb4666b45387c2b0b1d4440faecde4
Sean <xiamiliu@gmail.com>
2018-05-13 21:33:17 -0500

    BT or bitMap

--- src/com/ccsi/leetcode4/LC401BinaryWatch.java
+++ src/com/ccsi/leetcode4/LC401BinaryWatch.java
@@ -6,55 +6,44 @@ import java.util.*;
  * Created by gxliu on 2018/5/11.
  */
 public class LC401BinaryWatch {
-    private int[] hours={1,2,4,8};
-    private int[] minutes={1,2,4,8,16,32};
+    //method 1 backtracking
+
     public List<String> readBinaryWatch(int num){
         List<String> result=new ArrayList<>();
         if(num<0||num>=9)return result;
-        List<Integer> h=new ArrayList<>();
-        List<Integer> m=new ArrayList<>();
+
+        int[] hours={1,2,4,8};
+        int[] minutes={1,2,4,8,16,32};
         for (int i = 0; i <= num; i++) {
+            List<Integer> h=new ArrayList<>();
+            List<Integer> m=new ArrayList<>();
+
             if(i<4&&num-i<=6){ //限制一下条件，因为小时的个数不可能超过3个（4个时大于12了），分钟的个数不能超过5个（6个时大于60了）
                 //bt小时
-                backtracking(h,new ArrayList<Integer>(),hours,0,0,i,12);
+                backtracking(h,new ArrayList<Integer>(),hours,0,0,i);
                 //bt分钟，注意：小时和分钟LED灯的总个数是num
-                backtracking(m,new ArrayList<Integer>(),minutes,0,0,num-i,60);
-                if(i!=0&&h.isEmpty())continue;
-                if(num-i!=0&&m.isEmpty())continue;//有可能出现纯进去num-i个数（非0），但返回空的情况
-
-                if(h.isEmpty()&&m.isEmpty()){
-                    result.add("0:00");
-                }else if(h.isEmpty()){
-                    for(Integer mins:m){
-                        result.add(0+":"+(mins<10?"0"+mins:mins));
-                    }
-                }else if(m.isEmpty()){
-                    for(Integer hr:h){
-                        result.add(hr+":"+"00");
-                    }
-                }else{
-                    for(Integer hr:h){
-                        for(Integer mins:m){
-                            result.add(hr+":"+(mins<10?"0"+mins:mins));
-                        }
+                backtracking(m,new ArrayList<Integer>(),minutes,0,0,num-i);
+                for(Integer hour:h){
+                    if(hour>11)continue;
+                    for(Integer minute:m){
+                        if(minute>60)continue;
+                        result.add(hour+":"+(minute<10?"0"+minute:minute));
                     }
                 }
-                h.clear();
-                m.clear();
             }
         }
         return result;
     }
     //backtracking来找组合，条件是组合元素个数是固定的，其元素之和小于12小时或60分钟。
-    private void backtracking(List<Integer> bag,List<Integer> path,int[] array,int index,int curr,int number,int max){
-        if(path.size()>number||curr>=max)return; //个数超过目标个数或当前值大于最大值时就直接返回
+    private void backtracking(List<Integer> bag,List<Integer> path,int[] array,int index,int curr,int number){
+        if(path.size()>number)return; //个数超过目标个数或当前值大于最大值时就直接返回
         if(path.size()==number){ //如果个数是目标个数并且小于max时，做点事
             bag.add(curr);
         }else{
             for (int i = index; i < array.length; i++) {//否则就再加入下一个元素，递归来实现
                 path.add(i);
                 curr+=array[i];
-                backtracking(bag,path,array,i+1,curr,number,max);
+                backtracking(bag,path,array,i+1,curr,number);
                 path.remove(path.size()-1);  //递归完之后要去除最后加进去这个元素，bt典型的写法。
                 curr-=array[i];
             }
@@ -63,9 +52,36 @@ public class LC401BinaryWatch {
 
     public static void main(String[] args) {
         LC401BinaryWatch watch=new LC401BinaryWatch();
-        List<String> res=watch.readBinaryWatch(2);
+        List<String> res=watch.readBinaryWatch(8);
         for (String time:res) {
             System.out.println(time);
         }
+    }
+    //method 2 bitmap,通过二进制位中1的个数来判断是否符合，超时。
+    public List<String> readBinaryWatch1(int num){
+        List<String> result=new ArrayList<>();
+        if(num<0||num>9)return result;
+
+        int[] bitMap=new int[60];
+        for (int i = 0; i < 60; i++) {
+            int curr=i;
+            int count=0;
+            while(curr>0){
+                if((curr&1)==1)count++;
+                curr>>=1;
+            }
+            bitMap[i]=count;
+        }
+
+        for (int hour = 0; hour < 12; hour++) {
+            int currHourCount=bitMap[hour]; //0到11中每个数看一下二进制位中1的个数
+            for (int minute = 0; minute < 60; minute++) {
+                int currMinuteCount=bitMap[minute]; //0到60二进制中1的个数
+                if(currHourCount+currMinuteCount==num){ //如果两种个数总和等于num，那么这个时间就符合题目要求的
+                    result.add(hour+":"+(minute<10?"0"+minute:minute));
+                }
+            }
+        }
+        return result;
     }
 }
