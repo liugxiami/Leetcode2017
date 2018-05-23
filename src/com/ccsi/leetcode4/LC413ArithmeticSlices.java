6270f6ed7253bf015d8ee3cf15697469accceb1f
Sean <xiamiliu@gmail.com>
2018-05-22 23:01:38 -0500

    其实就是个等差数列的题，如果是等差数列，知道长度的情况下可以直接算出子等差数列（至少含3个元素）的个数，公式（len-1）*（len-2）/2，

---
+++ src/com/ccsi/leetcode4/LC413ArithmeticSlices.java
@@ -0,0 +1,54 @@
+package com.ccsi.leetcode4;
+
+/**
+ * Created by gxliu on 2018/5/22.
+ */
+public class LC413ArithmeticSlices {
+    public static void main(String[] args) {
+        int[] A={1,2,3,4,6,7,8,9};
+        System.out.println(numberOfArithmeticsSlices2(A));
+    }
+    public static int numberOfArithmeticsSlices(int[] A){
+        if(A.length<3)return 0;
+        int result=0;
+        int start=0;
+        int gap=A[1]-A[0];
+        int end=0;
+        for (int i = 1; i < A.length; i++) {
+            if(A[i]-A[i-1]==gap){
+                end=i;
+            }else{
+                //calculate
+                if(start+1<end){
+                    result+=helper(end-start+1);
+                }
+                start=i-1;
+                gap=A[i]-A[i-1];
+                end=i;
+            }
+        }
+        if(end==A.length-1&&start+1<end){
+            result+=helper(end-start+1);
+        }
+        return result;
+    }
+    private static int helper(int n){
+        int temp=n-2;
+        return (1+temp)*temp/2;
+    }
+    //method2 跟简洁一些
+    public static int numberOfArithmeticsSlices2(int[] A){
+        if(A.length<3)return 0;
+        int result=0;
+        int len=2;
+        for (int i = 2; i < A.length; i++) {
+            if(A[i]-A[i-1]==A[i-1]-A[i-2])len++;
+            else{
+                if(len>2)result+=helper(len);
+                len=2;
+            }
+        }
+        if(len>2)result+=helper(len);
+        return result;
+    }
+}
