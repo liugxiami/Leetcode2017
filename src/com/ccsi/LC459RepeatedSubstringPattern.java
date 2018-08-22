847c7aad1089168aaa0bf13ed773c870add02b17
Sean <xiamiliu@gmail.com>
2018-08-21 22:47:44 -0500

    分拆字符串，再组合起来看看和原字符串是否相同。

---
+++ src/com/ccsi/LC459RepeatedSubstringPattern.java
@@ -0,0 +1,23 @@
+package com.ccsi;
+
+public class LC459RepeatedSubstringPattern {
+    public static void main(String[] args) {
+        String s="abab";
+        System.out.println(repeatedSubstringPattern(s));
+    }
+    public static boolean repeatedSubstringPattern(String s){
+        if(s==null||s.length()<=1)return false;
+        int len=s.length();
+        for (int i = 1; i <= len / 2; i++) {
+            if(len%i==0){
+                int subLen=i;
+                String subStr=s.substring(0,subLen);
+                StringBuilder sb=new StringBuilder();
+                int repeats=len/subLen;
+                while(repeats-->0)sb.append(subStr);
+                if(sb.toString().equals(s))return true;
+            }
+        }
+        return false;
+    }
+}
