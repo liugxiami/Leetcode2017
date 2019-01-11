97c2294fd141d4968e019fba35437e475a445e6d
liugxiami <xiamiliu@gmail.com>
2019-01-10 22:11:53 -0600

    QuadTreeNode class

---
+++ src/com/ccsi/leetcode5/LC558QuadTreeIntersection.java
@@ -0,0 +1,4 @@
+package com.ccsi.leetcode5;
+
+public class LC558QuadTreeIntersection {
+}
---
+++ src/com/ccsi/leetcode5/QuadTreeNode.java
@@ -0,0 +1,19 @@
+package com.ccsi.leetcode5;
+
+public class QuadTreeNode {
+    public boolean val;
+    public boolean isLeaf;
+    public QuadTreeNode topLeft;
+    public QuadTreeNode topRight;
+    public QuadTreeNode bottomLeft;
+    public QuadTreeNode bottomRight;
+
+    public QuadTreeNode(boolean _val,boolean _isLeaf,QuadTreeNode _topLeft,QuadTreeNode _topRight,QuadTreeNode _bottomLeft,QuadTreeNode _bottomRight){
+        val=_val;
+        isLeaf=_isLeaf;
+        topLeft=_topLeft;
+        topRight=_topRight;
+        bottomLeft=_bottomLeft;
+        bottomRight=_bottomRight;
+    }
+}
