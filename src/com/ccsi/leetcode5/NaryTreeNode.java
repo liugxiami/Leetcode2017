f3f39e9c3a535ec43356c2eb8cb673ae9ccea719
liugxiami <xiamiliu@gmail.com>
2019-01-11 22:12:14 -0600

    NaryTreeNode

---
+++ src/com/ccsi/leetcode5/NaryTreeNode.java
@@ -0,0 +1,14 @@
+package com.ccsi.leetcode5;
+
+import java.util.*;
+
+public class NaryTreeNode {
+    public int val;
+    public List<NaryTreeNode> children;
+    public NaryTreeNode(){}
+
+    public NaryTreeNode(int _val,List<NaryTreeNode> _children){
+        this.val=val;
+        this.children=_children;
+    }
+}
