54928c122ddfc04e5c2ba984d871a561edc1e4a9
liugxiami <xiamiliu@gmail.com>
2019-01-15 21:30:26 -0600

    greedy and sort

---
+++ src/com/ccsi/leetcode5/LC56ArrayPartitionI.java
@@ -0,0 +1,19 @@
+package com.ccsi.leetcode5;
+
+import java.util.*;
+
+public class LC56ArrayPartitionI {
+    public static void main(String[] args) {
+        int[] nums={1,4,2,3,6,5};
+        System.out.println(arrayPairSum(nums));
+    }
+    public static int arrayPairSum(int[] nums){
+        if(nums==null||nums.length==0)return 0;
+        Arrays.sort(nums);
+        int sum=0;
+        for (int i = 0; i < nums.length; i+=2) {
+            sum+=nums[i];
+        }
+        return sum;
+    }
+}
